3e59775c8c7c7cf3784e39b708ab6d8f13f17f7c
John Atkinson <jgaxn0@gmail.com>
2017-02-05 23:34:02 -0700

    Add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks" 
+  }
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  
   root 'static_pages#home'
-  get 'static_pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
